22219c4fde52fe5cbabaf226a875a50750b0f588
Hakim Murphy <machiningdreams@gmail.com>
2023-06-29 13:04:21 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { CssBaseline } from '@mui/material'
+import TodoList from './TodoList'
+import Navbar from './Navbar'
+
+function App() {
+
+
+  return (
+    <>
+      <CssBaseline />
+      <Navbar />
+      <TodoList />
+    </>
+  )
+}
+
+export default App
